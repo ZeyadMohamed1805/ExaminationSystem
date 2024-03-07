c39b078dbe833d245f635050ee440106199c00f8
Amr Gamal <amr.aboulela97@gmail.com>
2024-03-07 22:00:32 +0200

    commit1

--- ExaminationSystem/ExaminationSystem/obj/Debug/net8.0/ExaminationSystem.AssemblyInfo.cs
+++ ExaminationSystem/ExaminationSystem/obj/Debug/net8.0/ExaminationSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExaminationSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bcee53b053e3e6c853f2efae1c2a99dbd69e65d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2691a9c4edf6fdf84343521347478e91da98903a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExaminationSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExaminationSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
